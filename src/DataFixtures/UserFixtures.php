192e328b6e48d8129a8488b81f54f254a7043272
Darren <nardol.darren@gmail.com>
2024-09-28 19:21:26 +0200

    add easyAdmin

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -22,13 +22,21 @@ class UserFixtures extends Fixture
         $user = new User();
         $user->setEmail('jhon_doe@gmail.com');
         $user->setRoles(['ROLE_ADMIN']);
-
         $password = $this->hasher->hashPassword($user, '123456');
         $user->setPassword($password);
-
         $user->setVerified(true);
 
+        $user2 = new User();
+        $user2->setEmail('test@gmail.com');
+        $user2->setRoles(['ROLE_USER']);
+        $password2 = $this->hasher->hashPassword($user2, 'test');
+        $user2->setPassword($password2);
+        $user2->setVerified(false);
+
+
+
         $manager->persist($user);
+        $manager->persist($user2);
 
         $manager->flush();
     }
