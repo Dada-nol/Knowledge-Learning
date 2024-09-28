52b7ce872d141fb772b52a1ecb18ae34ee5ec139
Darren <nardol.darren@gmail.com>
2024-09-28 16:13:58 +0200

    disable 'exclude Entity' in service

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +11,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
-        return $this->render('admin/index.html.twig');
+        $users = $em->getRepository(User::class)->findAll();
+
+
+        return $this->render('admin/index.html.twig', ['users' => $users]);
     }
 }
