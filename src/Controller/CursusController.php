9a5044c34ae483531be6eb068b482dbc6e754644
Darren <nardol.darren@gmail.com>
2024-10-03 19:31:39 +0200

    custom pages

--- src/Controller/CursusController.php
+++ src/Controller/CursusController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Cursus;
 use App\Entity\Lesson;
 use App\Entity\Theme;
+use App\Form\CursusType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -17,47 +19,31 @@ class CursusController extends AbstractController
     {
         $themes = $em->getRepository(Theme::class)->findAll();
 
-        return $this->render('Course/theme/index.html.twig', [
+        return $this->render('cursus/theme.html.twig', [
             'themes' => $themes,
         ]);
     }
 
-    #[Route('/theme/{id}', name: 'app_one_theme')]
-    public function oneTheme(EntityManagerInterface $em, int $id): Response
-    {
-        $theme = $em->getRepository(Theme::class)->find($id);
-
-        return $this->render('Course/theme/one.html.twig', [
-            'theme' => $theme,
-        ]);
-    }
 
     #[Route('/theme/{id}/cursus', name: 'app_cursus')]
-    public function allCursus(EntityManagerInterface $em, int $id): Response
+    public function allCursus(EntityManagerInterface $em, int $id, Request $request): Response
     {
         $theme = $em->getRepository(Theme::class)->find($id);
+        $form = $this->createForm(CursusType::class);
+        $form->handleRequest($request);
 
-        return $this->render('Course/cursus/index.html.twig', [
+        return $this->render('cursus/cursus.html.twig', [
             'theme' => $theme,
         ]);
     }
 
-    #[Route('/cursus/{id}', name: 'app_one_cursus')]
-    public function oneCursus(EntityManagerInterface $em, int $id): Response
-    {
-        $cursus = $em->getRepository(Cursus::class)->find($id);
-
-        return $this->render('Course/cursus/one.html.twig', [
-            'cursus' => $cursus,
-        ]);
-    }
 
     #[Route('/cursus/{id}/lesson', name: 'app_lesson')]
     public function allLesson(EntityManagerInterface $em, int $id): Response
     {
         $cursus = $em->getRepository(Cursus::class)->find($id);
 
-        return $this->render('Course/lesson/index.html.twig', [
+        return $this->render('cursus/lesson.html.twig', [
             'cursus' => $cursus,
         ]);
     }
@@ -67,7 +53,7 @@ class CursusController extends AbstractController
     {
         $lesson = $em->getRepository(Lesson::class)->find($id);
 
-        return $this->render('Course/lesson/one.html.twig', [
+        return $this->render('cursus/oneLesson.html.twig', [
             'lesson' => $lesson,
         ]);
     }
