a50d8ccdce279cee6790cace4551f654e8bed5f7
Darren <nardol.darren@gmail.com>
2024-10-23 17:40:10 +0200

    add PHPDoc

--- src/Controller/CertificationController.php
+++ src/Controller/CertificationController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Certificate;
-use App\Entity\Cursus;
 use App\Entity\Lesson;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
