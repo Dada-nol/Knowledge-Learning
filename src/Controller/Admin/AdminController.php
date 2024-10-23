ee4338f75af622cde9066cb63c9928c5c2269fd4
Darren <nardol.darren@gmail.com>
2024-10-23 19:53:22 +0200

    add annotation PHPDoc for all Controller and build he documentation

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -16,37 +16,37 @@ use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+/**
+ * Class AdminController
+ * @package App\Controller\Admin
+ * 
+ * This controller manages the admin dashboard and menu items for the application.
+ */
 class AdminController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
         return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
+    /**
+     * Configures the dashboard settings.
+     *
+     * @return Dashboard The configured dashboard.
+     */
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
             ->setTitle('Kwonledge Learning');
     }
 
+    /**
+     * Configures the menu items for the admin dashboard.
+     *
+     * @return iterable The menu items to be displayed.
+     */
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
