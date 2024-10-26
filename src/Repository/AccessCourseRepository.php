c09a87914a974753081ee5f28a08090d0c88e0d6
Darren <nardol.darren@gmail.com>
2024-10-26 22:53:26 +0200

    change dockerfile, apache.conf, env, accesscourse

--- src/Repository/AccessCourseRepository.php
+++ src/Repository/AccessCourseRepository.php
@@ -9,7 +9,7 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @extends ServiceEntityRepository<AccessCourse>
  */
-class UserCourseRepository extends ServiceEntityRepository
+class AccessCourseRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
